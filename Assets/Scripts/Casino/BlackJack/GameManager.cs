3815ed5af06ddee802a09e3a00d918628d24a36e
Veikka Luukkonen <61163039+Vegetius-s@users.noreply.github.com>
2024-06-12 14:17:01 +0300

    Blackjack beginnings :100:

---
+++ Assets/Scripts/Casino/BlackJack/GameManager.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Button dealButton;
+    public Button hitButton;
+    public Button standButton;
+    public Button betButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dealButton.onClick.AddListener(() => DealClicked());
+        hitButton.onClick.AddListener(() => HitClicked());
+        standButton.onClick.AddListener(() => StandClicked());
+    }
+    private void DealClicked()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void HitClicked()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void StandClicked()
+    {
+        throw new NotImplementedException();
+    }
+}

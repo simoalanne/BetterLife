897d5ed8ebc31e4f88243e31eba1bd898f6f6269
Veikka Luukkonen <61163039+Vegetius-s@users.noreply.github.com>
2024-07-26 14:08:31 +0300

    jotain legit

--- Assets/Scripts/GameOverCutscene.cs
+++ Assets/Scripts/GameOverCutscene.cs
@@ -2,16 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
 
 public class GameOverCutscene : MonoBehaviour
 {
-    [SerializeField] private Animator animator;
-    [SerializeField] public CanvasGroup canvasGroup;
-    [SerializeField] public AudioSource audioSource;
-    [SerializeField] public AudioClip[] audioClips;
-    [SerializeField] private float _transitionInDuration = 0.5f;
-    [SerializeField] private float _transitionOutDuration = 0.25f;
+    [SerializeField] private Animator animator; // Kidnappaus animaattori
+    [SerializeField] public CanvasGroup canvasGroup; // Fade mustaan
+    [SerializeField] public AudioSource audioSource; // Scenen audiosource
+    [SerializeField] public AudioClip[] audioClips; // Kaikki soundeffectit
+    [SerializeField] private float _transitionInDuration = 0.5f; // Kauanko fade in
+    [SerializeField] private float _transitionOutDuration = 0.25f; // Kauanko fade out
+    [SerializeField] public Image waterImage;
 
     private void Start()
     {
@@ -21,12 +22,12 @@ public class GameOverCutscene : MonoBehaviour
     private IEnumerator GameOver()
     {
         var timer = 0f;
-        animator.SetBool("Kidnapped", true);
-        audioSource.clip = audioClips[0];
-        audioSource.Play();
+        animator.SetBool("Kidnapped", true); // Soitetaan kidnappaus-animaatio
+        audioSource.clip = audioClips[0]; 
+        audioSource.Play(); // Ensimm‰inen ‰‰niefekti soitetaan
         yield return new WaitForSeconds(audioSource.clip.length);
 
-        while (timer < _transitionInDuration)
+        while (timer < _transitionInDuration) // Fade in
         {
             timer += Time.deltaTime;
             canvasGroup.alpha = timer / _transitionInDuration;
@@ -35,15 +36,16 @@ public class GameOverCutscene : MonoBehaviour
         canvasGroup.alpha = 1;
         animator.SetBool("Kidnapped", false);
 
-        for(int i = 1; i < audioClips.Length; i++)
+        for(int i = 1; i < audioClips.Length; i++) // Soitetaan sound effectit
         {
             audioSource.clip = audioClips[i];
             audioSource.Play();
             yield return new WaitForSeconds(audioSource.clip.length);
         }
-            
+        
+        waterImage.enabled = true;
         timer = 0f;
-        while (timer < _transitionOutDuration)
+        while (timer < _transitionOutDuration) // Fade out
         {
             timer += Time.deltaTime;
             canvasGroup.alpha = 1 - Mathf.Pow(timer / _transitionOutDuration, 3);
